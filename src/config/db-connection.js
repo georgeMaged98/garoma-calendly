92151821925fd49bd59cb3bf3fa3908e69cc0395
George Maged <g.maged327@gmail.com>
2022-09-13 20:02:17 +0200

    MONGO_URI in .env file

--- src/config/db-connection.js
+++ src/config/db-connection.js
@@ -2,16 +2,13 @@ const mongoose = require('mongoose')
 const DBError = require('../errors/DBError')
 
 const startDB = async () => {
-  mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.shird.mongodb.net/Calendly?retryWrites=true&w=majority`,
-    (err) => {
-      if (err) {
-        throw new DBError('Connection to Mongo FAILED!!')
-      }
-
-      console.log('Mongo Connected Successfully!!')
+  mongoose.connect(process.env.MONGO_URI, (err) => {
+    if (err) {
+      throw new DBError('Connection to Mongo FAILED!!')
     }
-  )
+
+    console.log('Mongo Connected Successfully!!')
+  })
 }
 
 module.exports = { startDB }
